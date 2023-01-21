50ea2cf075309222835e1e51fbb59626dca72a07
Benson Njuguna <njugunab655@gmail.com>
2023-01-21 22:50:46 +0300

    Create a sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,16 @@ export default function App () {
   return (
     <div className="container" >
       <h1>Notes App</h1>
-      <Note />
+      <div className="content">
+        <div className="sidebar">
+          {/* This will hold a summarized note */}
+          <Note />
+        </div>
+        <div className="details">
+          {/* This will hold a detailed note */}
+          <h2>description</h2>
+        </div>
+      </div>
     </div>
   )
 }
