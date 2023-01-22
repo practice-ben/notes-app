c55b766ea951e833e3cfdec54b84990f5522e5d9
Benson Njuguna <njugunab655@gmail.com>
2023-01-22 19:17:08 +0300

    Add note function

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import { useState } from "react";
 import Note from "./components/Note";
 import Form from "./components/Form";
 
 export default function App () {
+  // state to manage notes
+  const [notes, setNotes] = useState([]);
+  
+  function addNote(note) {
+    setNotes(prev => [...prev, note])
+  }
+
   return (
     <div className="container" >
       <h1>Notes App</h1>
-      <Form />
+      <Form addNote={addNote} />
       <div className="content">
         <div className="sidebar">
           {/* This will hold a summarized note */}
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 
-export default function Form() {
+export default function Form({addNote}) {
+    function respond(e) {
+        if(e.key === "Enter") {
+            addNote()
+        }
+    }
     return (
         <form>
-            <input type="text" placeholder="New Note" className="add-note" />
+            <input type="text" placeholder="New Note" className="add-note" onKeyUp={respond} />
         </form>
     )
 }
